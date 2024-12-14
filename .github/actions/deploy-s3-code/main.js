d00339f5cab326998e5ec5b882f45849b13299c0
srinath1 <srinath.erp@gmail.com>
2024-12-14 20:09:50 +0100

    uploading to s3

--- .github/actions/deploy-s3-code/main.js
+++ .github/actions/deploy-s3-code/main.js
@@ -3,6 +3,12 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
-  core.notice("Hello from custom action");
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+  
+
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 }
 run();
